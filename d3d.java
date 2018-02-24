5056205691e76cead31753722e92bcfbf491abab
Matthias Evering <36561948+ewingson@users.noreply.github.com>
2018-02-24 19:37:22 +0100

    Add files via upload
    exp

---
+++ d3d.java
@@ -0,0 +1,29 @@
+class d3d
+{
+	//main method, arguments may be passed on commandline execution
+	public static void main(String args[])
+	{
+		// declare and initiale 3D array
+		int arr[][][] = {
+						{ {0,4,2},{1,6,1},{5,4,2} },
+						{ {3,7,9},{9,6,2},{0,1,3} },
+						{ {5,8,4},{4,5,6},{2,5,2} }
+						};
+		// print 3D array in three nested for-loops
+		for (int i=0; i< 3 ; i++)
+		{
+			for (int j=0; j < 3 ; j++)
+			{
+				for (int k=0; k < 3; k++)
+				{
+					//print element
+					System.out.print(arr[i][j][k] + " ");
+				}
+				//print whitespaces
+				System.out.print("     ");
+			}
+			//newline
+			System.out.println();
+		}
+	}
+}
--- multiDimensional.java
+++ multiDimensional.java
@@ -1,18 +1,19 @@
-//classname must be filename
 class multiDimensional
 {
 	//main method, arguments may be passed on commandline execution
 	public static void main(String args[])
 	{
-		// declaring and initializing 2D array
-		int arr[][] = { {2,7,9},{3,6,1},{7,4,2} };
+		// declare and initialize 2D array
+		int arr[][] = { {1,5,9},{3,5,7},{0,6,2} };
 
-		// printing 2D array in two nested for-loops
-		for (int i=0; i< 3 ; i++)
+		// print 2D array in two nested for-loops
+		for (int i = 0; i < 3 ; i++)
 		{
-			for (int j=0; j < 3 ; j++)
+			for (int j = 0; j < 3 ; j++)
+			{
 				//print element
 				System.out.print(arr[i][j] + " ");
+			}
 			//newline
 			System.out.println();
 		}
